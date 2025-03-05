a1af8cc5811bf4a6108fbf1bf71ac97bfcfd81b2
collins muriuki <muriukicollins48@gmail.com>
2025-03-05 16:15:31 +0300

    declared variables and assigned them a string

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = "Alice"; // or whatever user you're working with
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+let shortGreeting = `Welcome, ${currentUser.charAt(0)}!`; // First initial
